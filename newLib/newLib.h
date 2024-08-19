42696c847abdbbe13cb209f767381e86fea94901
Harshit Kushwah <harshitkushwah@Administrators-MacBook-Pro.local>
2024-08-19 11:22:39 +0530

    setup

---
+++ newLib/newLib.h
@@ -0,0 +1,18 @@
+//
+//  newLib.h
+//  newLib
+//
+//  Created by Harshit Kushwah on 19/08/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for newLib.
+FOUNDATION_EXPORT double newLibVersionNumber;
+
+//! Project version string for newLib.
+FOUNDATION_EXPORT const unsigned char newLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newLib/PublicHeader.h>
+
+
---
+++ newLibExample/newLibExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  newLibExampleApp.swift
+//  newLibExample
+//
+//  Created by Harshit Kushwah on 19/08/24.
+//
+
+import SwiftUI
+
+@main
+struct newLibExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
